09dd458f2ea6c112769325576d2e7185e5ad9014
HanHyeonBin <63350603+HanHyeonBin@users.noreply.github.com>
2022-02-12 13:42:36 +0900

    commit

--- React/begin-react/src/App.js
+++ React/begin-react/src/App.js
@@ -1,23 +1,8 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import Hello from "./Hello";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>안녕하세요.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
