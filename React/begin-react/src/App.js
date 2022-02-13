56ab71ea4f7e9f610f43735d6d7c295fb970aab5
HanHyeonBin <63350603+HanHyeonBin@users.noreply.github.com>
2022-02-13 13:36:48 +0900

    commit

--- React/begin-react/src/App.js
+++ React/begin-react/src/App.js
@@ -1,13 +1,25 @@
 import React from "react";
 import Hello from "./Hello";
+import "./App.css";
 
 function App() {
+  const name = "react";
+  const style = {
+    backgroundColor: "black",
+    color: "aqua",
+    fontSize: 24,
+    padding: "1rem",
+  };
   return (
-    <div>
-      <Hello />
-      <Hello />
-      <Hello />
-    </div>
+    <>
+      {/* JSX 주석 사용법은 대괄호 + 별 슬래시 */}
+      <Hello
+      // 이건또 되네;
+      />
+      <div style={style}>{name}</div>{" "}
+      {/* JS 값을 보여줘야 할때는 {}로 묶어준다 */}
+      <div className="gray-box"></div>
+    </>
   );
 }
 
--- React/begin-react/src/Hello.js
+++ React/begin-react/src/Hello.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Hello() {
-  return <div>안녕하세요</div>;
+  return <div>Hello World</div>;
 }
 
 export default Hello;
