bbafbd6679f35c0adbbc12ec5f5de920fb73e5af
HanHyeonBin <63350603+HanHyeonBin@users.noreply.github.com>
2022-02-14 20:11:47 +0900

    input 상태관리

---
+++ React/begin-react/src/InputSample.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+function InputSample() {
+  const [text, setText] = useState("");
+  const onChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const onReset = () => {
+    setText("");
+  };
+  return (
+    <div>
+      <input onChange={onChange} value={text} />
+      <button onClick={onReset}>초기화</button>
+      <div>
+        <b>값 : </b>
+        {text}
+      </div>
+    </div>
+  );
+}
+
+export default InputSample;
