b6e013fb49228364b25122de4e4f1daa95a2edf4
HanHyeonBin <63350603+HanHyeonBin@users.noreply.github.com>
2022-02-19 17:22:38 +0900

    useRef

--- React/begin-react/src/App.js
+++ React/begin-react/src/App.js
@@ -1,8 +1,34 @@
-import React from "react";
+import React, { useRef } from "react";
 import UserList from "./UserList";
 
 function App() {
-  return <UserList />;
+  const users = [
+    // 배열을 만들때 Key값을 줘야 나중에 추가,삭제 등 관리할때 용이하다.
+    {
+      id: 1,
+      username: "Han",
+      email: "123@gmail.com",
+    },
+    {
+      id: 2,
+      username: "Hyeon",
+      email: "456@gmail.com",
+    },
+    {
+      id: 3,
+      username: "Bin",
+      email: "789@gmail.com",
+    },
+  ];
+
+  const nextId = useRef(4);
+
+  const onCreate = () => {
+    console.log(nextId.current); //4
+    nextId.current += 1;
+  };
+
+  return <UserList users={users} />;
 }
 
 export default App;
--- React/begin-react/src/UserList.js
+++ React/begin-react/src/UserList.js
@@ -9,26 +9,7 @@ function User({ user }) {
   );
 }
 
-function UserList() {
-  const users = [
-    // 배열을 만들때 Key값을 줘야 나중에 추가,삭제 등 관리할때 용이하다.
-    {
-      id: 1,
-      username: "Han",
-      email: "123@gmail.com",
-    },
-    {
-      id: 2,
-      username: "Hyeon",
-      email: "456@gmail.com",
-    },
-    {
-      id: 3,
-      username: "Bin",
-      email: "789@gmail.com",
-    },
-  ];
-
+function UserList({ users }) {
   return (
     <div>
       {users.map((user) => (
