b6a0e1a91674d4d11447b1732a2cc167cab584ba
HanHyeonBin <63350603+HanHyeonBin@users.noreply.github.com>
2022-02-19 17:44:59 +0900

    배열에 항목 추가(concat함수)

--- React/begin-react/src/App.js
+++ React/begin-react/src/App.js
@@ -1,8 +1,21 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import UserList from "./UserList";
+import CreateUser from "./CreateUser";
 
 function App() {
-  const users = [
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: "",
+  });
+  const { username, email } = inputs;
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+  const [users, setUsers] = useState([
     // 배열을 만들때 Key값을 줘야 나중에 추가,삭제 등 관리할때 용이하다.
     {
       id: 1,
@@ -19,16 +32,36 @@ function App() {
       username: "Bin",
       email: "789@gmail.com",
     },
-  ];
+  ]);
 
   const nextId = useRef(4);
 
   const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email,
+    };
+    setUsers(users.concat(user));
+    setInputs({
+      username: "",
+      email: "",
+    });
     console.log(nextId.current); //4
     nextId.current += 1;
   };
 
-  return <UserList users={users} />;
+  return (
+    <>
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList users={users} />
+    </>
+  );
 }
 
 export default App;
