937ad01d6cc1a81e71ff022ba60eedaac6628359
HanHyeonBin <63350603+HanHyeonBin@users.noreply.github.com>
2022-03-20 16:08:12 +0900

    customHook

--- React/begin-react/src/App.js
+++ React/begin-react/src/App.js
@@ -1,6 +1,7 @@
 import React, { useRef, useReducer, useMemo, useCallback } from "react";
 import UserList from "./UserList";
 import CreateUser from "./CreateUser";
+import useInputs from "./useInputs";
 
 function counActiveUsers(users) {
   console.log("활성 사용자 수를 세는중...");
@@ -8,10 +9,6 @@ function counActiveUsers(users) {
 }
 
 const initialState = {
-  inputs: {
-    username: "",
-    email: "",
-  },
   users: [
     {
       id: 1,
@@ -36,14 +33,6 @@ const initialState = {
 
 function reducer(state, action) {
   switch (action.type) {
-    case "CHANGE_INPUT":
-      return {
-        ...state,
-        inputs: {
-          ...state.inputs,
-          [action.name]: action.value,
-        },
-      };
     case "CREATE_USER":
       return {
         inputs: initialState.inputs,
@@ -68,18 +57,13 @@ function reducer(state, action) {
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
+  const [form, onChange, reset] = useInputs({
+    username: "",
+    email: "",
+  });
+  const { username, email } = form;
   const nextId = useRef(4);
   const { users } = state;
-  const { username, email } = state.inputs;
-
-  const onChange = useCallback((e) => {
-    const { name, value } = e.target;
-    dispatch({
-      type: "CHANGE_INPUT",
-      name,
-      value,
-    });
-  });
 
   const onCreate = useCallback(() => {
     dispatch({
@@ -91,7 +75,8 @@ function App() {
       },
     });
     nextId.current += 1;
-  }, [username, email]);
+    reset();
+  }, [username, email, reset]);
 
   const onToggle = useCallback((id) => {
     dispatch({
