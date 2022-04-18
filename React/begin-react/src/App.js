b80d0ab234172a82a72095c1645420635602cfe3
HanHyeonBin <63350603+HanHyeonBin@users.noreply.github.com>
2022-04-18 20:37:14 +0900

    Immer 라이브러리

--- React/begin-react/src/App.js
+++ React/begin-react/src/App.js
@@ -43,22 +43,33 @@ const initialState = {
 function reducer(state, action) {
   switch (action.type) {
     case "CREATE_USER":
-      return {
-        inputs: initialState.inputs,
-        users: state.users.concat(action.user),
-      };
+      return produce(state, (draft) => {
+        draft.users.push(action.user);
+      });
+    // return {
+    //   inputs: initialState.inputs,
+    //   users: state.users.concat(action.user),
+    // };
     case "TOGGLE_USER":
-      return {
-        ...state,
-        users: state.users.map((user) =>
-          user.id === action.id ? { ...user, active: !user.active } : user
-        ),
-      };
+      return produce(state, (draft) => {
+        const user = draft.users.find((user) => user.id === action.id);
+        user.active = !user.active;
+      });
+    // return {
+    //   ...state,
+    //   users: state.users.map((user) =>
+    //     user.id === action.id ? { ...user, active: !user.active } : user
+    //   ),
+    // };
     case "REMOVE_USER":
-      return {
-        ...state,
-        users: state.users.filter((user) => user.id !== action.id),
-      };
+      return produce(state, (draft) => {
+        const index = draft.users.findIndex((user) => user.id === action.id);
+        draft.users.splice(index, 1);
+      });
+    // return {
+    //   ...state,
+    //   users: state.users.filter((user) => user.id !== action.id),
+    // };
     default:
       throw new Error("Unhandled action");
   }
